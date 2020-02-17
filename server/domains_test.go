d58713bc1da17ad94e65b8550d9dac2a7fb27017
AGMETEOR <guwats10@gmail.com>
2020-02-17 14:19:47 +0300

    Add a table test structure

--- server/domains_test.go
+++ server/domains_test.go
@@ -13,21 +13,37 @@ func (s *DoPluginTestSuite) TestListDomainsCommandFunc() {
 	p := &Plugin{BotUserID: "1"}
 	api := &plugintest.API{}
 
-	commandArgs := &model.CommandArgs{Command: "/jira settings", UserId: "1"}
-	s.Run("Test List Domains when there no available domains", func() {
-		s.client.EXPECT().ListDomains(context.TODO(), nil).Return([]godo.Domain{}, &godo.Response{}, nil).Times(1)
+	commandArgs := &model.CommandArgs{Command: "/jira list-domains", UserId: "1"}
 
-		api.On("LogError", mock.AnythingOfTypeArgument("string")).Return(nil)
-		api.On("SendEphemeralPost", mock.AnythingOfType("string"), mock.AnythingOfType("*model.Post")).Run(func(args mock.Arguments) {
-			post := args.Get(1).(*model.Post)
-			s.Assert().Equal("You don't have any domains configured. Use `/do create-domain  <domainName> <ipAddress[optional]>` to provision one", post.Message)
-		}).Once().Return(&model.Post{})
+	tests := map[string]struct {
+		message         string
+		domains         []godo.Domain
+		commandResponse *model.CommandResponse
+	}{
+		"Test List Domains when there no available domains": {
+			message:         "You don't have any domains configured. Use `/do create-domain  <domainName> <ipAddress[optional]>` to provision one",
+			domains:         []godo.Domain{},
+			commandResponse: &model.CommandResponse{},
+		},
+	}
 
-		p.SetAPI(api)
+	for name, tt := range tests {
+		s.Run(name, func() {
+			s.client.EXPECT().ListDomains(context.TODO(), nil).Return(tt.domains, &godo.Response{}, nil).Times(1)
 
-		response, err := p.listDomainsCommandFunc(s.client, commandArgs)
+			api.On("LogError", mock.AnythingOfTypeArgument("string")).Return(nil)
+			api.On("SendEphemeralPost", mock.AnythingOfType("string"), mock.AnythingOfType("*model.Post")).Run(func(args mock.Arguments) {
+				post := args.Get(1).(*model.Post)
+				s.Assert().Equal(tt.message, post.Message)
+			}).Once().Return(&model.Post{})
 
-		s.Require().Nil(err)
-		s.Require().Equal("", response.Text)
-	})
+			p.SetAPI(api)
+
+			response, err := p.listDomainsCommandFunc(s.client, commandArgs)
+
+			s.Require().Nil(err)
+			s.Require().Equal(tt.commandResponse.Text, response.Text)
+		})
+
+	}
 }
