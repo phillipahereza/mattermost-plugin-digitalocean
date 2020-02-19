fb0c7e5c014be5375122651c7e340cca2d46f3d3
AGMETEOR <guwats10@gmail.com>
2020-02-19 21:47:16 +0300

    Delete commented code

--- server/cron.go
+++ server/cron.go
@@ -8,30 +8,11 @@ import (
 // RunJobs is
 func (p *Plugin) RunJobs() {
 	c := cron.New()
-
-	// post := &model.Post{
-	// 	UserId:    p.BotUserID,
-	// 	Message:   "I am bot NS",
-	// 	ChannelId: "43gik9wr17nzijea5h655jxxiy",
-	// }
-
-	// postx := &model.Post{
-	// 	UserId:    p.BotUserID,
-	// 	Message:   "I am bot NS",
-	// 	ChannelId: "3piif5x1x7r4bkmgiju5wuqf7y",
-	// }
-
-	// c.AddFunc("*/1 * * * *", func() {
-	// 	p.API.SendEphemeralPost("mijye5mke7dbdbi8fb3b3a8bwh", post)
-	// 	p.API.SendEphemeralPost("mijye5mke7dbdbi8fb3b3a8bwh", postx)
-	// })
-
 	c.AddFunc("*/1 * * * *", func() {
 		p.PostToChannels()
 	})
 
 	c.Start()
-
 }
 
 // PostToChannels is
