373f5423948f0437fdb0d5b80b77595cd53e3a05
AGMETEOR <guwats10@gmail.com>
2020-02-21 12:55:49 +0300

    Use slack attachment

--- server/cron.go
+++ server/cron.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/digitalocean/godo"
 	"github.com/mattermost/mattermost-server/v5/model"
@@ -18,9 +19,35 @@ func (p *Plugin) RunPollingJobs() {
 	c.Start()
 }
 
+func (p *Plugin) getSlackAttachment(inactive []godo.Droplet) []*model.SlackAttachment {
+	var fields []*model.SlackAttachmentField
+
+	if len(inactive) > 0 {
+		fields = append(fields, &model.SlackAttachmentField{
+			Title: "Droplet regular check",
+			Value: fmt.Sprintf("%d were found inactive", len(inactive)),
+			Short: true,
+		})
+
+	} else {
+		fields = append(fields, &model.SlackAttachmentField{
+			Title: "Droplet regular check",
+			Value: "Add a message here. All droplets alive and kickin'",
+			Short: true,
+		})
+	}
+	return []*model.SlackAttachment{
+		{
+			Color:  "#95b7d0",
+			Text:   "",
+			Fields: fields,
+		},
+	}
+}
+
 // PollForDroplets is
 func (p *Plugin) PollForDroplets() ([]godo.Droplet, error) {
-	client, err := p.GetClient("pass_user_id")
+	client, err := p.GetClient("mijye5mke7dbdbi8fb3b3a8bwh")
 	if err != nil {
 		return []godo.Droplet{}, err
 	}
@@ -31,12 +58,11 @@ func (p *Plugin) PollForDroplets() ([]godo.Droplet, error) {
 	return droplets, nil
 }
 
-func checkDroplets(droplets []godo.Droplet) *model.Post {
+func (p *Plugin) checkDroplets(droplets []godo.Droplet) (ok bool, inactive []godo.Droplet) {
 	var inactiveDroplets []godo.Droplet
 
 	if len(droplets) == 0 {
-		// TODO: Compose better message
-		return &model.Post{}
+		return true, []godo.Droplet{}
 	}
 
 	for _, droplet := range droplets {
@@ -46,20 +72,26 @@ func checkDroplets(droplets []godo.Droplet) *model.Post {
 		inactiveDroplets = append(inactiveDroplets, droplet)
 	}
 
-	// TODO: Compose post with information after the polling
-	return &model.Post{}
+	if len(inactiveDroplets) > 0 {
+		return false, inactiveDroplets
+	}
+
+	return true, []godo.Droplet{}
 }
 
 // PostToChannels is
 func (p *Plugin) PostToChannels() {
 	subcription, _ := p.store.LoadSubscription()
+	droplets, _ := p.PollForDroplets()
+	_, inactive := p.checkDroplets(droplets)
 	channels := subcription.Channels
 	for _, channel := range channels {
+		attachment := p.getSlackAttachment(inactive)
 		post := &model.Post{
 			UserId:    p.BotUserID,
-			Message:   "I am bot NS",
 			ChannelId: channel,
 		}
+		post.AddProp("attachments", attachment)
 
 		p.API.SendEphemeralPost("mijye5mke7dbdbi8fb3b3a8bwh", post)
 	}
